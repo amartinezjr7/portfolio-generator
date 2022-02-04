d16ec1a2da4310b92a7dfc93b19755b10bb0f76b
amartinezjr7 <94876426+amartinezjr7@users.noreply.github.com>
2022-02-03 22:53:44 -0800

    Generate HTML File

---
+++ src/page-template.js
@@ -0,0 +1,19 @@
+const generatePage = (name, github) => {
+    return `
+    <!DOCTYPE html>
+    <html lang = "en">
+    <head>
+        <meta charset = "UTF-8">
+        <meta name="viewpoint" content="width=device-width, initial-scale=1.0">
+        <meta https-equiv="X-UA-Compatable" content="ie=edge">
+    </head>
+    
+    <body>
+        <h1>${name}</h1>
+        <h2><a href="https://github.com/${github}">Github</a></h2>    
+    </body>
+    `;    
+    
+};
+
+module.exports=generatePage;
